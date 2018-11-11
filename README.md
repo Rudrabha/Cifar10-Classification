29eeb578e80d481c7dca7b17948f160e325e06aa
rudrabha <rudrabha@gmail.com>
2018-11-11 10:36:37 +0530

    Added few functions

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Cifar10-Classification
+Requirements- 
+python 3.6.6
+Scikit-learn 0.20.0
+keras 2.2.2
+tensorflow 1.9.0
+
+The dataset files should be downloaded and put in cifar10 directory.
+
+To run a classifier - 
+
+python main.py --classifier DT --depth 3 --representation MLP_Embedding 
+python main.py --clasifier SVM --C 200.0 --representation LDA 
+python main.py --classifier LR --eta 0.001 --epochs 100 --batch_size 1000 
+python main.py --classifier MLP --eta 0.0001 --epochs 10000 --batch_size 100 --dropout 0.2
+
+All the above mentioned parameters have some default value.
+
+The PCA would by default use 350 number of components. To use some other number, use the --ncomponents option along with --representation PCA
+
+At present, I have pre calculated LDA and PCA on the training data and saved that in the data folder. To recalculate it while running a classifier, 
+
+python main.py --create_dim_red PCA
+python main.py --create_dim_red LDA
--- main.py
+++ main.py
@@ -13,9 +13,16 @@ from keras.callbacks import ModelCheckpoint
 from keras.regularizers import l2
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--create_dim_red", help="To create new dimension reduced data.", default=None)
+parser.add_argument("--create_dim_red", help="To create new dimension reduced data.", default="", choices=["PCA, LDA"])
 parser.add_argument("--representation", help="Which represenatation to use", default="LDA", choices=["Raw_Data", "PCA", "LDA", "MLP_Embedding"])
 parser.add_argument("--classifier", help="Which classifier to use, DT=Decision Tree, SVM, MLP, LR=Logistic Regression", default="DT", choices=["DT", "SVM", "LR", "MLP"])
+parser.add_argument("--eta", type=float, help="Learning Rate, used only for MLP and Logistic Regression", default=0.0001)
+parser.add_argument("--C", type=float, help="Used for only linear SVM (soft margin)", default=500.0)
+parser.add_argument("--depth", type=int, help="Used only for decision trees, maxdepth", default=9)
+parser.add_argument("--epochs", type=int, help="Used only for Logistic regression and MLP. Specifies the number of epochs", default=10)
+parser.add_argument("--batch_size", type=int, help="Used only for Logistic regression and MLP. Specifies the batch size", default=5000)
+parser.add_argument("--dropout", type=float, help="Used only for MLP. Specifies the dropout amount (Same for all the layers)", default=0.2)
+parser.add_argument("--ncomponents", type=int, help="The number of components from pca (max 350)", default=350)
 args = parser.parse_args()
 
 
@@ -28,6 +35,7 @@ def select_representation(option):
 		val_data = transform_pca(val_data, pca_train)
 		trn_data = transform_pca(trn_data, pca_train) 
 		tst_data = transform_pca(tst_data, pca_train)
+		return trn_data[:,0:args.ncomponents], trn_labels, val_data[:,0:args.ncomponents], val_labels, tst_data[:,0:args.ncomponents], tst_labels
 
 	elif option=="LDA":
 		trn_data, trn_labels, val_data, val_labels, tst_data, tst_labels = load_cifar()
@@ -53,7 +61,7 @@ def dimensionality_reduction(option):
 
 def classifier(option, trn_data, trn_labels, val_data, val_labels, tst_data, tst_labels):
 	if option=="DT":
-		svr = tree.DecisionTreeClassifier(max_depth=9)
+		svr = tree.DecisionTreeClassifier(max_depth=args.depth)
 		print ("Training Decision Tree")
 		svr.fit(trn_data, trn_labels)
 		print ("Predicting from model")
@@ -61,7 +69,7 @@ def classifier(option, trn_data, trn_labels, val_data, val_labels, tst_data, tst
 		acc, f1  = calculate_scores(tst_labels, pred)
 		return acc, f1
 	if option=="SVM":
-		svr = svm.SVC(C=500.0, kernel="linear")
+		svr = svm.SVC(C=args.C, kernel="linear")
 		print ("Training SVM")
 		svr.fit(trn_data, trn_labels)
 		print ("Predicting from model")
@@ -74,25 +82,25 @@ def classifier(option, trn_data, trn_labels, val_data, val_labels, tst_data, tst
 		model = Sequential()
 		layer1 = Dense(units = 1000, activation = 'relu', input_dim = trn_data.shape[1])
 		model.add(layer1)
-		model.add(Dropout(0.2))
+		model.add(Dropout(args.dropout))
 		layer2 = Dense(units = 500, activation = 'relu')
 		model.add(layer2)
-		model.add(Dropout(0.2))
+		model.add(Dropout(args.dropout))
 		layer3 = Dense(units = 250, activation = 'relu')
 		model.add(layer3)
-		model.add(Dropout(0.2))
+		model.add(Dropout(args.dropout))
 		layer4 = Dense(units = 125, activation = 'relu')
 		model.add(layer4)
-		model.add(Dropout(0.2))
+		model.add(Dropout(args.dropout))
 		layer5 = Dense(units = 50, activation = 'relu')
 		model.add(layer5)
-		model.add(Dropout(0.2))
+		model.add(Dropout(args.dropout))
 		layer6 = Dense(units = 10, activation = 'softmax')
 		model.add(layer6)
-		optimizer = Adam(lr=1e-4)
+		optimizer = Adam(lr=args.eta)
 		model.summary()
 		model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics = ['accuracy'])
-		model.fit(trn_data, trn_labels, batch_size=5000, epochs=10, shuffle=True, validation_data=val_split)
+		model.fit(trn_data, trn_labels, batch_size=args.batch_size, epochs=args.epochs, shuffle=True, validation_data=val_split)
 		acc = model.evaluate(tst_data, tst_labels)
 		return acc[1], acc[1]
 	if option=="LR":
@@ -100,13 +108,14 @@ def classifier(option, trn_data, trn_labels, val_data, val_labels, tst_data, tst
 		val_split = (tst_data, tst_labels)
 		model = Sequential()
 		model.add(Dense(10, activation='softmax', input_dim=trn_data.shape[1]))
-		model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-		model.fit(trn_data, trn_labels, batch_size=5000, epochs=10, shuffle=True, validation_data=val_split)
+		optimizer = Adam(lr=args.eta)
+		model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+		model.fit(trn_data, trn_labels, batch_size=args.batch_size, epochs=args.epochs, shuffle=True, validation_data=val_split)
 		acc = model.evaluate(tst_data, tst_labels)
 		return acc[1], acc[1]
 
 if args.create_dim_red=="PCA" or args.create_dim_red=="LDA":
-	dimensionality_reduction(dim_red)
+	dimensionality_reduction(args.create_dim_red)
 trn_data, trn_labels, val_data, val_labels, tst_data, tst_labels = select_representation(args.representation)
 acc, f1 = classifier(args.classifier, trn_data, trn_labels, val_data, val_labels, tst_data, tst_labels)
 print ("Accuracy = "+ str(acc))
